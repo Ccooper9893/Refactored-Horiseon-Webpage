2088f01d84789bce7a7d347e0f76e4afd8327fbf
Cody Cooper <cody_codster@live.com>
2022-11-07 14:19:33 -0600

    Added Javascript

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+console.log('- Replaced non-semantic HTML with semantic elements to give more meaning the the webpage elements and provide better accessibility.');
+console.log('- Consolidated CSS code by ridding the file of repeating code.');
+console.log('- Tested resulting page for functionality and appearance to make sure the refactored webpage is identical to the provided mock-up.');
